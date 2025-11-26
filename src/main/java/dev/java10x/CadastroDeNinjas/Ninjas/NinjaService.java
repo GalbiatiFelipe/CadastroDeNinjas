afbe37774859852b7876fa816e90ad0b7403c614
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-11-26 20:12:47 -0300

    feat: cria camada de service com primeiro metodo para listar todos os elementos cadastrados no banco de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,27 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// Annotation que transforma a classe em um Service
+@Service
 public class NinjaService {
+
+    /*
+    * Injetar dependencia Repository para que possamos acessar os métodos do JPA
+    * 1- criar atributo do tipo 'repository'
+    * 2- inicializar construtor
+    * */
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar todos os meus ninjas
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
 }
