11dfa42fba3d5a50b81e81857623cc183a9d1620
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2025-11-27 10:10:22 -0300

    style: adiciona comentarios em MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,18 +1,26 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// Annotation que transforma a classe em um Service
 @Service
 public class MissoesService {
 
+    /*
+     * Injetar dependencia Repository para que possamos acessar os métodos do JPA
+     * 1- criar atributo do tipo 'repository'
+     * 2- inicializar construtor
+     * */
     private MissoesRepository missoesRepository;
 
     public MissoesService(MissoesRepository missoesRepository) {
         this.missoesRepository = missoesRepository;
     }
 
+    // Listar todas as missoes
     public List<MissoesModel> listarMissoes() {
         return missoesRepository.findAll();
     }
